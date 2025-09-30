4e20a8fb7312797e5fa005458ec8bfce83eb08a8
ouwargui <guikemon2@gmail.com>
2025-09-30 01:04:31 -0300

    feat: preparing to support plugins

--- Sources/BetterAuth/Models/Session.swift
+++ Sources/BetterAuth/Models/Session.swift
@@ -50,6 +50,8 @@ public struct User: Codable, Sendable {
   public let createdAt: Date
   public let updatedAt: Date
 
+  package let pluginData: [String: AnyCodable]?
+
   public init(
     id: String,
     email: String,
@@ -57,7 +59,8 @@ public struct User: Codable, Sendable {
     image: String?,
     emailVerified: Bool,
     createdAt: Date,
-    updatedAt: Date
+    updatedAt: Date,
+    pluginData: [String: AnyCodable]? = nil
   ) {
     self.id = id
     self.email = email
@@ -66,5 +69,73 @@ public struct User: Codable, Sendable {
     self.emailVerified = emailVerified
     self.createdAt = createdAt
     self.updatedAt = updatedAt
+    self.pluginData = pluginData
+  }
+
+  private enum CodingKeys: String, CodingKey, CaseIterable {
+    case id, email, name, image, emailVerified, createdAt, updatedAt
+  }
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+
+    id = try container.decode(String.self, forKey: .id)
+    email = try container.decode(String.self, forKey: .email)
+    name = try container.decode(String.self, forKey: .name)
+    image = try container.decodeIfPresent(String.self, forKey: .image)
+    emailVerified = try container.decode(Bool.self, forKey: .emailVerified)
+    createdAt = try container.decode(Date.self, forKey: .createdAt)
+    updatedAt = try container.decode(Date.self, forKey: .updatedAt)
+
+    let dynamicContainer = try decoder.container(
+      keyedBy: DynamicCodingKeys.self
+    )
+    var unknownFields: [String: AnyCodable] = [:]
+
+    let knownKeys = Set(CodingKeys.allCases.map { $0.rawValue })
+
+    for key in dynamicContainer.allKeys {
+      if !knownKeys.contains(key.stringValue) {
+        let value = try dynamicContainer.decode(AnyCodable.self, forKey: key)
+        unknownFields[key.stringValue] = value
+      }
+    }
+
+    pluginData = unknownFields.isEmpty ? nil : unknownFields
+  }
+
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+
+    try container.encode(id, forKey: .id)
+    try container.encode(email, forKey: .email)
+    try container.encode(name, forKey: .name)
+    try container.encodeIfPresent(image, forKey: .image)
+    try container.encode(emailVerified, forKey: .emailVerified)
+    try container.encode(createdAt, forKey: .createdAt)
+    try container.encode(updatedAt, forKey: .updatedAt)
+
+    if let pluginData = pluginData {
+      var dynamicContainer = encoder.container(keyedBy: DynamicCodingKeys.self)
+      for (key, value) in pluginData {
+        let codingKey = DynamicCodingKeys(stringValue: key)!
+        try dynamicContainer.encode(value, forKey: codingKey)
+      }
+    }
+  }
+
+  private struct DynamicCodingKeys: CodingKey {
+    var stringValue: String
+    var intValue: Int?
+
+    init?(stringValue: String) {
+      self.stringValue = stringValue
+      self.intValue = nil
+    }
+
+    init?(intValue: Int) {
+      self.stringValue = String(intValue)
+      self.intValue = intValue
+    }
   }
 }
